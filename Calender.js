a804b532ab29b100f94e7e1eafe7ad0f31f58972
Ayesha1375 <55167625+Ayesha1375@users.noreply.github.com>
2019-09-29 23:38:07 -0400

    Add files via upload

---
+++ Calender.js
@@ -0,0 +1,14 @@
+<script>
+    function myFunction() {
+    alert("Congragulations on being born 😊 !")
+    }
+</script>
+
+
+<script>
+        $(document).ready(function(){
+          $("button2").click(function(){
+            $("p2").text("Too many can kill you!!! Hahahaha");
+          })
+        });
+</script>
